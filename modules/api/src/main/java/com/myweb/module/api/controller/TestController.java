153864b911f23a64991e1ab420f26c4f00a23d0f
ChenHongshuai <c2238034790@163.com>
2022-08-02 16:20:49 +0800

    添加日志功能

--- modules/api/src/main/java/com/myweb/module/api/controller/TestController.java
+++ modules/api/src/main/java/com/myweb/module/api/controller/TestController.java
@@ -2,6 +2,7 @@ package com.myweb.module.api.controller;
 
 import com.myweb.common.core.exception.ServiceException;
 import com.myweb.common.core.result.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author chs
  * @date 2022/7/18 17:01
  */
+@Slf4j
 @RestController
 @RequestMapping(value = "/test")
 public class TestController {
@@ -21,6 +23,18 @@ public class TestController {
             throw new ServiceException("i = 1");
         }
         Result<String> success = Result.success("hello");
+        log.error("asdk");
+        log.info("asdk");
+        log.debug("asdk");
+        log.info("asdk");
         return success;
+
+    }
+
+    public static void main(String[] args) {
+        log.error("asdk");
+        log.info("asdk");
+        log.debug("asdk");
+        log.info("asdk");
     }
 }
